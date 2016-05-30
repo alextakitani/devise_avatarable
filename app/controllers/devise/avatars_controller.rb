cfffb289dc788a27167a1a0d92a779fad4d57723
Alex Takitani <atpires@gmail.com>
2016-05-30 15:33:02 -0300

    Update avatars_controller.rb
    Debugging

--- app/controllers/devise/avatars_controller.rb
+++ app/controllers/devise/avatars_controller.rb
@@ -20,6 +20,7 @@ class Devise::AvatarsController < DeviseController
         respond_with resource, location: after_update_path_for(resource)
       end
     else
+      Rails.logger.error resource.errors
       respond_with_navigational(resource){ render :edit }
     end
   end
